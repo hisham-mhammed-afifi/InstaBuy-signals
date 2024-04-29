6c6e983ed5f09e2c899361f49c0241564ac85774
hesham-afifi <hesham.mohamed@widebot.net>
2024-04-29 10:28:23 +0300

    transform Observables to signals

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../models/Product';
@@ -7,24 +7,33 @@ import { ProductBackendService } from './services/product-backend.service';
 import { SearchComponent } from './search/search.component';
 import { FilterComponent } from './filter/filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [AsyncPipe, SearchComponent, FilterComponent, ProductCardComponent],
+  imports: [
+    AsyncPipe,
+    JsonPipe,
+    SearchComponent,
+    FilterComponent,
+    ProductCardComponent,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent implements OnInit {
-  products$!: Observable<Product[]>;
+  products = this.productsService.getAllProducts();
   categories$!: Observable<string[]>;
 
   selectedCategory = 'all';
 
-  constructor(private productsBackend: ProductBackendService) {}
+  constructor(
+    private productsBackend: ProductBackendService,
+    private productsService: ProductService
+  ) {}
 
   ngOnInit(): void {
-    this.products$ = this.productsBackend.getAllProducts();
     this.categories$ = this.productsBackend.getAllCategories();
   }
 
--- src/app/modules/products/services/product.service.ts
+++ src/app/modules/products/services/product.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ProductBackendService } from './product-backend.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private productsBackend: ProductBackendService) {}
 
-  constructor() { }
+  getAllProducts() {
+    const products$ = this.productsBackend.getAllProducts();
+    return toSignal(products$, { initialValue: [] });
+  }
 }
